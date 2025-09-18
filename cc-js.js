e592c6dc251f5cec84974b8ef29737e118a51135
badcarolwork <85211809+badcarolwork@users.noreply.github.com>
2025-09-19 00:05:12 +0800

    v2.1

--- cc-js.js
+++ cc-js.js
@@ -19,10 +19,10 @@ function showKPanelBg(state){
   const panels = document.querySelectorAll('.panel');
   panels.forEach(panel => {
       if(state){
-        panel.style.backgroundImage = 'url("purebg.jpg")'; 
+        panel.style.backgroundImage = 'url(left.png), url(purebg.jpg)'; 
         panel.style.backgroundRepeat = 'no-repeat';
-        panel.style.backgroundPosition = 'center top';
-        panel.style.backgroundSize = '2300px 1314px';
+        panel.style.backgroundPosition = 'center 50%, center top';
+        panel.style.backgroundSize = '94% auto, 2300px 1314px';
         bgApplied = 'true'; // prevent reapplying
       }else{
         panel.style.backgroundImage = 'unset'; 
