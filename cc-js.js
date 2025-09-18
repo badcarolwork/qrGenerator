cdcb9e5511593faf2a88bd77aaf7e3d7597f5f7d
badcarolwork <85211809+badcarolwork@users.noreply.github.com>
2025-09-18 13:59:06 +0800

    updatedv1

--- cc-js.js
+++ cc-js.js
@@ -1,7 +1,28 @@
-function handleOnScroll(){        
-  window.addEventListener("message", function (event) {
-    if (event.data) {
-      console.log(event.data);
-    }
+ 
+let bgApplied = false;
+     
+window.addEventListener("message", function (event) {
+  if (event.data) {
+    console.log(event.data);
+  }
+});
+
+
+function showKPanelBg(){
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach(panel => {
+      if(state){
+        panel.style.backgroundImage = 'url("purebg.jpg")'; 
+        panel.style.backgroundRepeat = 'no-repeat';
+        panel.style.backgroundPosition = 'center top';
+        panel.style.backgroundSize = '2300px 1314px';
+        bgApplied = 'true'; // prevent reapplying
+      }else{
+        panel.style.backgroundImage = 'unset'; 
+        panel.style.backgroundRepeat = 'no-repeat';
+        panel.style.backgroundPosition = 'unset';
+        panel.style.backgroundSize = 'unset';
+        bgApplied = 'false'; // prevent reapplying
+      }
   });
 }
