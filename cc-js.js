ed49d0ad51a02d99f47db80e5afd1d103719a617
badcarolwork <85211809+badcarolwork@users.noreply.github.com>
2025-09-19 08:45:23 +0800

    v2.3

--- cc-js.js
+++ cc-js.js
@@ -3,12 +3,11 @@ let bgApplied = false;
      
 window.addEventListener("message", function (event) {
  const message = event.data;
- console.log(event.data.data.text)
   if (message) {
-    if(event.data.data.text === "kscrollReveal"){
+    if(message.data.text === "kscrollReveal"){
        showKPanelBg(true); 
     }
-   if(event.data.data.text === "kscrollTop"){
+   if(message.data.text === "kscrollTop"){
       showKPanelBg(false);
     }
   }
@@ -17,19 +16,18 @@ window.addEventListener("message", function (event) {
 
 function showKPanelBg(state){
   const panels = document.querySelector('.panel.top');
-  panels.forEach(panel => {
-      if(state && !bgApplied){
-        panel.style.backgroundImage = 'url(purebg.jpg)'; 
-        panel.style.backgroundRepeat = 'no-repeat';
-        panel.style.backgroundPosition = 'center top';
-        panel.style.backgroundSize = '2300px 1314px';
-        bgApplied = 'true'; // prevent reapplying
-      }else if(!state && bgApplied){
-        panel.style.backgroundImage = 'unset'; 
-        panel.style.backgroundRepeat = 'no-repeat';
-        panel.style.backgroundPosition = 'unset';
-        panel.style.backgroundSize = 'unset';
-        bgApplied = 'false'; // prevent reapplying
-      }
-  });
+     if(state && !bgApplied){
+       panel.style.backgroundImage = 'url(purebg.jpg)'; 
+       panel.style.backgroundRepeat = 'no-repeat';
+       panel.style.backgroundPosition = 'center top';
+       panel.style.backgroundSize = '2300px 1314px';
+       bgApplied = 'true'; // prevent reapplying
+     }else if(!state && bgApplied){
+       panel.style.backgroundImage = 'unset'; 
+       panel.style.backgroundRepeat = 'no-repeat';
+       panel.style.backgroundPosition = 'unset';
+       panel.style.backgroundSize = 'unset';
+       bgApplied = 'false'; // prevent reapplying
+     }
+
 }
